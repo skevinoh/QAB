99ff9aa2fec21fcfa1a5b84063455a200df7d662
skevinoh <soh2@oxy.edu>
2018-12-06 14:34:33 -0800

    Initial Commit

---
+++ QAB_proj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QAB_proj
+//
+//  Created by Kevin Sejin Oh on 12/6/18.
+//  Copyright © 2018 Kevin Sejin Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
