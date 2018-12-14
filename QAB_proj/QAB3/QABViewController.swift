ca33e0c0409211e848219054a2502d3401878cd8
skevinoh <soh2@oxy.edu>
2018-12-13 17:12:19 -0800

    variables

--- QAB_proj/QAB3/QABViewController.swift
+++ QAB_proj/QAB3/QABViewController.swift
@@ -16,6 +16,69 @@ class QABViewController: UIViewController {
         
     }
     
+    
+    /*
+     Math for QAB stats:
+     
+     //QAB TOTALS
+     var pitchingQABTotal : Int
+     var bigInnQABTotal : Int
+     var fiftyPercentQABTotal : Int
+     var leadoffQABTotal : Int
+     var twostrikesQABTotal : Int
+     var twooutsQABTotal: Int
+     
+     //INN TOTALS
+     var atbatsInnTotal : Int
+     var inningInnTotal : Int
+     var qualityInnTotal : Int
+     var pitchesInnTotal : Int
+     var stringsInnTotal : Int
+     var runsInnTotal : Int
+     
+     //QAB STATS
+     var count : Int
+     var pitches : Int
+     var pitchType : String()
+     var contact : Int
+     var result : String()
+     var qualityorno : String()
+     var chases : String()
+     
+     
+     
+     
+     //INDIVIDUAL TOTALS
+     var batterOnePitches: Int
+     var batterOneQuality: Int
+     var batterOneContact: Int
+     var batterTwoPitches: Int
+     var batterTwoQuality: Int
+     var batterTwoContact: Int
+     var batterThreePitches: Int
+     var batterThreeQuality: Int
+     var batterThreeContact: Int
+     var batterFourPitches: Int
+     var batterFourQuality: Int
+     var batterFourContact: Int
+     var batterFivePitches: Int
+     var batterFiveQuality: Int
+     var batterFiveContact: Int
+     var batterSixPitches: Int
+     var batterSixQuality: Int
+     var batterSixContact: Int
+     var batterSevenPitches: Int
+     var batterSevenQuality: Int
+     var batterSevenContact: Int
+     var batterEightPitches: Int
+     var batterEightQuality: Int
+     var batterEightContact: Int
+     var batterNinePitches: Int
+     var batterNineQuality: Int
+     var batterNineContact: Int
+     
+    */
+    
 
     /*
     // MARK: - Navigation
